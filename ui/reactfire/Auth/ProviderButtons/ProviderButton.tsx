582eadfb94f62da8cde0f4351c007ac6a89a3753
Webber <webber@takken.io>
2022-01-04 19:33:03 +0100

    feat: add twitter and facebook buttons

--- ui/reactfire/Auth/ProviderButtons/ProviderButton.tsx
+++ ui/reactfire/Auth/ProviderButtons/ProviderButton.tsx
@@ -3,14 +3,29 @@ import { useState } from 'react';
 import { signInWithPopup } from 'firebase/auth';
 import { loadingDelay } from '../../../../utils/loadingDelay';
 import Spinner from 'core/ui/react-icons/Spinner';
+import DynamicIcon from '../../../react-icons/DynamicIcon';
+import getIconPropsForProviderId from '../getIconPropsForProviderId';
+import { AuthProvider } from 'firebase/auth';
+import { useNotification } from '../../../../hooks/useNotification';
 
-const ProviderButton = ({ name, provider, icon }) => {
+interface Props {
+  name: string;
+  provider: AuthProvider;
+}
+
+const ProviderButton = ({ name, provider }: Props) => {
   const auth = useAuth();
+  const notify = useNotification();
+
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const signInWithProvider = async () => {
     try {
-      const action = signInWithPopup(auth, provider);
+      const action = notify.promise(signInWithPopup(auth, provider), {
+        loading: 'Waiting for response from identity provider.',
+        error: (error) => `Unable to sign in. ${error}`,
+        success: `Signed in.`,
+      });
       const result = await Promise.race([loadingDelay(), action]);
 
       if (result === 'loading') {
@@ -30,10 +45,12 @@ const ProviderButton = ({ name, provider, icon }) => {
     }
   };
 
+  const providerIcon = <DynamicIcon {...getIconPropsForProviderId(provider.providerId)} />;
+
   return (
     <div>
       <button className="social" onClick={signInWithProvider}>
-        {isLoading ? <Spinner /> : icon}
+        {isLoading ? <Spinner /> : providerIcon}
         <span>{name}</span>
       </button>
     </div>
