43782ea12edcb8d111adf52cfbef179ac2cd6f01
Webber <webber@takken.io>
2022-01-07 02:12:29 +0100

    chore: dispatch action on update profile

--- ui/reactfire/Auth/Profile/ProfileIconAndDisplayName/ProfileIconAndDisplayName.tsx
+++ ui/reactfire/Auth/Profile/ProfileIconAndDisplayName/ProfileIconAndDisplayName.tsx
@@ -5,6 +5,9 @@ import ProfileIcon from '../Icon/ProfileIcon';
 import { useCallback, useState } from 'react';
 import { debounce } from '../../../../../utils/debounce';
 import { useNotification } from '../../../../../hooks/useNotification';
+import { useDispatch } from 'react-redux';
+// Todo - remove nuclear option
+import { profileUpdated } from '../../../../../../user/logic/ducks/profileDuck';
 
 interface Props {}
 
@@ -12,15 +15,17 @@ function ProfileIconAndDisplayName({}: Props): JSX.Element {
   const { data: user }: { data: User } = useUser();
   const [displayName, setDisplayName] = useState<string>(user.displayName);
   const notify = useNotification();
+  const dispatch = useDispatch();
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const persistDisplayName = useCallback(
-    debounce((displayName) => {
-      notify.promise(updateProfile(user, { displayName }), {
+    debounce(async (displayName) => {
+      await notify.promise(updateProfile(user, { displayName }), {
         loading: 'Updating display name',
         success: 'Display name updated',
         error: 'Display name was not updated',
       });
+      await dispatch(profileUpdated({ displayName }));
     }, 500),
     [user, debounce],
   );
